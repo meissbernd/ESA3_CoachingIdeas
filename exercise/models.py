a584ee2692d92bb545b313db98d402e9946f87c8
Bernd Meissner <bernd.meissner@gmx.de>
2023-12-03 15:49:11 +0100

    add Creator and Exercise to model

--- exercise/models.py
+++ exercise/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Creator(models.Model):
+    """Person that creates trainings content (e.g. exercises)."""
+
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name}"
+
+
+class Exercise(models.Model):
+    """Physical exercise for training."""
+
+    description = models.CharField(max_length=40)
+    creator = models.ForeignKey(Creator, on_delete=models.SET_NULL, null=True)
