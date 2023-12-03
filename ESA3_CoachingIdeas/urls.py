c3060fc4db33b0f760afa62c19976d27a3ac0580
Bernd Meissner <bernd.meissner@gmx.de>
2023-12-03 17:22:23 +0100

    show list of exercises

--- ESA3_CoachingIdeas/urls.py
+++ ESA3_CoachingIdeas/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from exercise.views import get_exercise_list
 from helloworld.views import get_hello_world
 
 urlpatterns = [
     path("helloworld/", get_hello_world),
+    path("exercises/", get_exercise_list, name="exercise_list"),
     path("admin/", admin.site.urls),
 ]
--- exercise/views.py
+++ exercise/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from exercise.models import Exercise
+
+
 # Create your views here.
+
+
+def get_exercise_list(request):
+    exercises = Exercise.objects.all().order_by("description")
+    response = render(
+        request,
+        template_name="exercise/exercise_list.html",
+        context={"page_title": "Trainingsübungen", "exercises": exercises},
+    )
+    return response
