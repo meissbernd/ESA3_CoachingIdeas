cd6351d0ba909df81317f0ee7fcc4f63b8cf374e
Bernd Meissner <bernd.meissner@rolls-royce.com>
2023-12-22 17:05:01 +0000

    register models for admin

--- exercise/admin.py
+++ exercise/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from exercise.models import AgeGroup
+from exercise.models import Creator
+from exercise.models import Exercise
+from exercise.models import SoccerSkill
+
 # Register your models here.
+
+admin.site.register(SoccerSkill)
+admin.site.register(Exercise)
+admin.site.register(AgeGroup)
+admin.site.register(Creator)
