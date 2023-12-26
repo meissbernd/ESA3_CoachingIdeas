554d5f1263c1ad76068ed02a6bbcbc66dc267154
Bernd Meissner <bernd.meissner@gmx.de>
2023-12-26 08:24:51 +0100

    age group as boolean

--- exercise/admin.py
+++ exercise/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 
-from exercise.models import AgeGroup
 from exercise.models import Creator
 from exercise.models import Exercise
 
-# from exercise.models import SoccerSkillTextChoices
 
 # Register your models here.
 
 # admin.site.register(SoccerSkillTextChoices)
 admin.site.register(Exercise)
-admin.site.register(AgeGroup)
 admin.site.register(Creator)
--- exercise/models.py
+++ exercise/models.py
@@ -24,35 +24,6 @@ class SoccerSkillTextChoices(models.TextChoices):
     COOPERATION = "Zusammenspiel"
 
 
-# class SoccerSkill(models.Model):
-#     """Specific skill that can be addressed by exercises."""
-#
-#     skill = models.CharField(max_length=50, choices=SoccerSkillTextChoices.choices)
-#
-#     def __str__(self):
-#         return f"{self.skill}"
-
-
-class AgeGroup(models.Model):
-    """Specific age group that can be addressed by exercises."""
-
-    AGE_GROUPS = (
-        ("s", "Erwachsene"),
-        ("a", "A-Jun"),
-        ("b", "B-Jun"),
-        ("c", "C-Jun"),
-        ("d", "D-Jun"),
-        ("e", "E-Jun"),
-        ("f", "F-Jun"),
-        ("g", "G-Jun"),
-    )
-
-    age_group = models.CharField(max_length=1, choices=AGE_GROUPS)
-
-    def __str__(self):
-        return f"{self.age_group}"
-
-
 class Exercise(models.Model):
     """Physical exercise for training."""
 
@@ -64,8 +35,14 @@ class Exercise(models.Model):
     soccer_skills = models.CharField(
         max_length=50, choices=SoccerSkillTextChoices.choices, null=True
     )
-
-    # age_groups = models.ManyToManyField(AgeGroup, related_name="exercises")
+    age_adults = models.BooleanField(default=True)
+    age_jun_a = models.BooleanField(default=False)
+    age_jun_b = models.BooleanField(default=False)
+    age_jun_c = models.BooleanField(default=False)
+    age_jun_d = models.BooleanField(default=False)
+    age_jun_e = models.BooleanField(default=False)
+    age_jun_f = models.BooleanField(default=False)
+    age_jun_g = models.BooleanField(default=False)
 
     # def __str__(self):
     #     return f"{self.title}_{self.soccer_skills.get_skill_display()}"
