7026f1aa0281bddc6d8c880516d31ec68625bbb6
Bernd Meissner <bernd.meissner@gmx.de>
2023-12-27 16:52:18 +0100

    alter basic functional test to unit test

--- functional_test.py
+++ functional_test.py
@@ -1,15 +1,19 @@
+import unittest
 from selenium import webdriver
 
 
-def test_functional_basic():
-    """Test if main page of app starts."""
-    browser = webdriver.Firefox()
-    browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-    assert "Trainingsübungen" in browser.title
-    print("OK")
-    browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_exercise_list(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("Trainingsübungen", self.browser.title)
+        # self.fail("Finish the test!")
 
 
 if __name__ == "__main__":
-    test_functional_basic()
+    unittest.main()
