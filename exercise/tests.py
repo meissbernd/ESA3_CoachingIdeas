4758cd105c9e446b3cf3f61367a62456a651e7b5
Bernd Meissner <bernd.meissner@gmx.de>
2023-12-27 18:09:01 +0100

    add ExerciseHomeViewTest

--- exercise/tests.py
+++ exercise/tests.py
@@ -1,9 +1,13 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.http import HttpRequest
+from exercise.views import ExerciseHome
 
 
-# Create your tests here.
-
-
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class ExerciseHomeViewTest(TestCase):
+    def test_html_contains_correct_title(self):
+        request = RequestFactory().get("/")
+        view = ExerciseHome()
+        view.setup(request)
+        response = ExerciseHome.as_view()(request)
+        html = response.rendered_content
+        self.assertIn("<title>Trainingsübungen</title>", html)
