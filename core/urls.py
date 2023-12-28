55cd94e117e954b055b40d73b1afc32fe4c0ab8c
Bernd Meissner <bernd.meissner@gmx.de>
2023-12-28 10:03:08 +0100

    upload and store files with the FileSystemStorage

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from core import views
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path("", views.Home.as_view(), name="home"),
     path("upload/", views.upload, name="upload"),
 ]
+
+# add a path to core/media (to have access to local stored files)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
 
 # Create your views here.
@@ -10,12 +11,15 @@ class Home(TemplateView):
 
 
 def upload(request):
-    # context = {}
+    context = {}
     if request.method == "POST":
         uploaded_file = request.FILES["document"]
-        print(uploaded_file.name)
-        print(uploaded_file.size)
-    #     fs = FileSystemStorage()
-    #     name = fs.save(uploaded_file.name, uploaded_file)
-    #     context["url"] = fs.url(name)
-    return render(request, "core/upload.html")  # , context)
+        # print(uploaded_file.name)
+        # print(uploaded_file.size)
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        # url = fs.url(name)
+        # print(url)
+        # Put url of uploaded file into context, send to template and use there
+        context["url"] = f"/core{fs.url(name)}"
+    return render(request, "core/upload.html", context)
