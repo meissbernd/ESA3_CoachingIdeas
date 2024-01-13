272281da3021b696907d7bc6b3d0546449cd2a5d
Bernd Meissner <bernd.meissner@gmx.de>
2024-01-13 18:19:38 +0100

    fix basic unit test

--- exercise/tests.py
+++ exercise/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import http
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class GetExerciseListTest(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=True)
+
+    def test_home_page_can_be_called(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, http.HTTPStatus.FOUND)
