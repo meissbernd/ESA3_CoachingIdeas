026c0794b700066d793b6a33211123104e490221
Bernd Meissner <bernd.meissner@gmx.de>
2024-01-20 13:27:59 +0100

    Make Selenium functional test running and let start test server automatically

--- functional_test.py
+++ functional_test.py
@@ -1,18 +1,29 @@
 import unittest
+
+from django.test import LiveServerTestCase
 from selenium import webdriver
 
 
-class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+class TestLandingPage(LiveServerTestCase):
+    """Test landing page can be called."""
 
-    def tearDown(self):
-        self.browser.quit()
+    browser = None
 
-    def test_can_start_app(self):
-        self.browser.get("http://localhost:8000/")
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.browser = webdriver.Firefox()
+        cls.browser.implicitly_wait(3)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+    def test_server_starts_and_landing_page_is_up(self):
+        """Verify that landing page can be reached on the server."""
+        self.browser.get(f"{self.live_server_url}")
         self.assertIn("ESA3 CoachingIdeas", self.browser.title)
-        # self.fail("Finish the test!")
 
 
 if __name__ == "__main__":
